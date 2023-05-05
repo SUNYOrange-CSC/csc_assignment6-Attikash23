3058877b37dc96f0f21949a3eb32fb432c93ecb4
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-05-05 13:49:57 -0400

    Add files via upload

---
+++ Ch6_race_assignment.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*int num(int num);
+
+int main(){
+int result;
+  result = num(4);
+  cout << result << endl;
+  return 0;
+}
+int num (int num){
+  return num * num* num;
+
+}*/
+/*
+void ten_times (int number) {
+cout << number*10<< endl;
+}*/
+
+/*double gravity = 9.8; 
+double fallDis(double);
+
+int main()
+{
+	double d,
+  t;
+	for (int i = 0; i < 10; i++)
+    {
+		t = i + 1;
+        d = fallDis(t);
+        cout << t << " secs =  " << d << " meters" << endl;
+    }
+  	return 0;
+}
+double fallDis(double falling_time)
+{
+    return (0.5) * gravity * pow(falling_time, 2);
+}*/
+
